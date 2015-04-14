80bea26ffcbdd1c89846a6f59a71dd2eb109216b
evghenii <egaburov@dds.nl>
2015-04-14 09:13:03 +0200

    templated conversion

--- helper/cvt_m2n.cu
+++ helper/cvt_m2n.cu
@@ -6,8 +6,9 @@ using real_t = double;
 using fluid_t = State<real_t, FluidStates::NSTATES,15>;
 
 
-__global__ void 
-convert_modal2nodal(const fluid_t& stateModal, fluid_t& stateNodal)
+template<bool NODAL2MODAL>
+__device__ void 
+convert_modal2nodal2modal(const fluid_t& in, fluid_t& out)
 {
   const int zoneIdx  = blockIdx.x*blockDim.x + threadIdx.x;
   const int stateIdx = blockIdx.y;
@@ -18,40 +19,35 @@ convert_modal2nodal(const fluid_t& stateModal, fluid_t& stateNodal)
 #pragma unroll
   for (int s = 0; s < N; s++)
   {
-    b[s] = stateModal[stateIdx](zoneIdx, s);
+    b[s] = in[stateIdx](zoneIdx, s);
   }
 
+
+  if (!NODAL2MODAL)
+  {
 #include "m2n.h"
+  }
+  else
+  {
+#include "n2m.h"
+  }
   
 #pragma unroll
   for (int s = 0; s < N; s++)
   {
-    stateNodal[stateIdx](zoneIdx, s) = x[s];
+    out[stateIdx](zoneIdx, s) = x[s];
   }
 }
 
 
 
 __global__ void 
+convert_modal2nodal(const fluid_t& stateModal, fluid_t& stateNodal)
+{
+  convert_modal2nodal2modal<0>(stateModal, stateNodal);
+}
+__global__ void 
 convert_nodal2modal(const fluid_t& stateNodal, fluid_t& stateModal)
 {
-  const int zoneIdx  = blockIdx.x*blockDim.x + threadIdx.x;
-  const int stateIdx = blockIdx.y;
-
-  constexpr int N = fluid_t::n_coeff;
-  real_t x[N], b[N];
-
-#pragma unroll
-  for (int s = 0; s < N; s++)
-  {
-    b[s] = stateNodal[stateIdx](zoneIdx, s);
-  }
-
-#include "n2m.h"
-  
-#pragma unroll
-  for (int s = 0; s < N; s++)
-  {
-    stateModal[stateIdx](zoneIdx, s) = x[s];
-  }
+  convert_modal2nodal2modal<1>(stateNodal, stateModal);
 }
