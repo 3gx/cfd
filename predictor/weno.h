1d0812ab99f5af2be4ce9b87af3619cf4f15ce31
evghenii <egaburov@dds.nl>
2015-04-28 11:49:07 +0200

    started on weno5

--- predictor/weno.h
+++ predictor/weno.h
@@ -1,3 +1,17 @@
 #pragma once
 
+#include "common.h"
+
+template<typename real_type, typename EOS_type>
+struct WenoFD5
+{
+  using value_type = real_type;
+  using EOS = EOS_type;
+
+  using Cell = CellT<real_type,EOS>;
+
+  std::vector<Cell::Conservative> cons_vec;
+
+};
+
 
