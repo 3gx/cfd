9621df9ee7751d95254c1472decc1949e991ff91
evghenii <egaburov@dds.nl>
2015-05-16 17:56:08 +0200

    type renames

--- misc_algo/diffusion_euler.cpp
+++ misc_algo/diffusion_euler.cpp
@@ -7,21 +7,22 @@
 #include "common.h"
 
 
-template<typename real_t>
-struct Params
+template<typename Real>
+struct ParamT
 {
-  using value_type = real_t;
-  real_t diff;
-  real_t dx;
-  real_t cfl;
+  using value_type = Real;
+  Real time;
+  Real cfl;
+  Real dx;
+  Real diff;
 
-  real_t dt() const {return cfl * diff/square(dx);}
+  Real dt() const {return cfl * diff/square(dx);}
 };
 
 struct PeriodicBC
 {
-  template<typename vector_t>
-    static void apply(vector_t &f) 
+  template<typename Vector>
+    static void apply(Vector &f) 
     {
       const auto n = f.size();
       f[0  ] = f[n-2];
@@ -31,8 +32,8 @@ struct PeriodicBC
 
 struct FreeBC 
 {
-  template<typename vector_t>
-    static void apply(vector_t &f) 
+  template<typename Vector>
+    static void apply(Vector &f) 
     {
       const auto n = f.size();
       f[0  ] = f[  1];
@@ -40,28 +41,28 @@ struct FreeBC
     }
 };
 
-template<typename param_t, typename vector_t>
-static void compute_df(const param_t &params, const vector_t &f, vector_t &df)
+template<typename Param, typename Vector>
+static void compute_df(const Param &params, const Vector &f, Vector &df)
 {
-  using real_t = typename vector_t::value_type;
+  using Real = typename Vector::value_type;
   const auto c = params.diff/square(params.dx);
   const int n = f.size();
   for (int i = 1 ; i < n-1; i++)
   {
-    df[i] = c * (f[i+1] - real_t{2.0} * f[i] + f[i-1]);
+    df[i] = c * (f[i+1] - Real{2.0} * f[i] + f[i-1]);
   }
 }
 
-template<typename BC,typename param_t, typename vector_t>
-static void compute_update(const param_t &params, vector_t &f)
+template<typename BC,typename Param, typename Vector>
+static void compute_update(const Param &params, Vector &f)
 {
-  using real_t = typename vector_t::value_type;
+  using Real = typename Vector::value_type;
   const auto n = f.size();
 
   BC::apply(f);
   
 
-  static vector_t df(n);
+  static Vector df(n);
 
   compute_df(params,f,df);
 
@@ -75,18 +76,18 @@ static void compute_update(const param_t &params, vector_t &f)
   BC::apply(f);
 }
 
-template<typename BC, typename param_t, typename vector_t>
-static void compute_update_dg1(const param_t &params, vector_t &f)
+template<typename BC, typename Param, typename Vector>
+static void compute_update_dg1(const Param &params, Vector &f)
 {
-  using real_t = typename vector_t::value_type;
+  using Real = typename Vector::value_type;
   const auto n = f.size();
   const auto dt = params.dt();
 
-  const vector_t f0 = f;
+  const Vector f0 = f;
   const int niter = 10;
   for (int iter = 0; iter < niter; iter++)
   {
-    static vector_t df(n);
+    static Vector df(n);
     BC::apply(f);
     compute_df(params,f,df);
     for (int i = 1; i < n-1; i++)
@@ -99,10 +100,10 @@ static void compute_update_dg1(const param_t &params, vector_t &f)
 
 }
 
-template<typename param_t, typename vector_t>
-void set_ic(const param_t &params, vector_t &f)
+template<typename Param, typename Vector>
+void set_ic(const Param &params, Vector &f)
 {
-  using real_t = typename vector_t::value_type;
+  using Real = typename Vector::value_type;
   using std::max;
 
   /* set  a profile of delta function */
@@ -112,32 +113,32 @@ void set_ic(const param_t &params, vector_t &f)
   const auto L = dx*(n-2);
   const auto ic = n>>1;
 
-  const auto ampl = real_t{1.0};
+  const auto ampl = Real{1.0};
 
 
   const int di = 10;
   std::fill(f.begin(), f.end(), 0);
   for (int i = -di; i <= di; i++)
   {
-    const auto fi = max(ampl*(di - abs(i)),real_t{0});
+    const auto fi = max(ampl*(di - abs(i)),Real{0});
     f[ic + i] = fi;
   }
   std::fill(f.begin(), f.end(), 0);
   for (int i = -di; i <= di; i++)
   {
-    const auto fi = max(ampl*(di - abs(i)),real_t{0});
+    const auto fi = max(ampl*(di - abs(i)),Real{0});
     f[ic - ic/2 + i] = fi;
   }
   for (int i = -di; i <= di; i++)
   {
-    const auto fi = max(ampl*(di - abs(i)),real_t{0});
+    const auto fi = max(ampl*(di - abs(i)),Real{0});
     f[ic + ic/3 + i] = fi;
   }
 
 }
 
-template<typename vector_t>
-void dump2file(const vector_t &f, const std::string fileName)
+template<typename Vector>
+void dump2file(const Vector &f, const std::string fileName)
 {
   const int n = f.size();
   std::ofstream fout(fileName);
@@ -154,18 +155,19 @@ int main(int argc, char * argv[])
   printf(std::cerr, "niter= %\n", niter);
   
 
-  using real_t = double;
+  using Real = double;
 
-  using params_t = Params<real_t>;
-  using vector_t = std::vector<real_t>;
+  using Param = ParamT<Real>;
+  using Vector = std::vector<Real>;
 
-  auto params = params_t{};
+  auto params = Param{};
   params.dx   = 1;
   params.diff = 1;
   params.cfl  = 0.40;  /* stable for cfl <= 0.5 */
+  params.time = 0;
 //  params.cfl  = 0.252;
 
-  vector_t f(ncell+2);
+  Vector f(ncell+2);
 
   set_ic(params,f);
   dump2file(f,"ic.txt");
