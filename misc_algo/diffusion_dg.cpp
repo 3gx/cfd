ec37f5e614ce29429841e01fcab252da183ffeb5
evghenii <egaburov@dds.nl>
2015-05-19 09:48:56 +0200

    added Expansion class and basic itertor method

--- misc_algo/diffusion_dg.cpp
+++ misc_algo/diffusion_dg.cpp
@@ -6,106 +6,103 @@
 #include "zip_iterator.h"
 #include "common.h"
 
-template<size_t N, typename Real>
-struct DGMatrixBase
+template<size_t N, typename T, typename Real>
+class ExpansionBase
 {
-  using value_type = Real;
-  using array = std::array<Real,N>;
-  using matrix = std::array<array,N>;
-  static constexpr auto size() { return N; }
+  protected:
+    using vector_type = std::array<Real,N>;
+    using matrix_type = std::array<vector_type,N>;
+    std::array<T,N> data;
+  public:
+    using value_type = T;
+    static constexpr auto size() { return N; }
+    T& operator[](const size_t i) {return data[i];}
+    const T& operator[](const size_t i) const {return data[i];}
 };
 
+template<size_t N, typename T, typename Real>
+class Expansion;
 
-template<size_t N, typename Real> struct DGMatrix;
-
-template<typename Real>
-struct DGMatrix<1,Real> : public DGMatrixBase<1,Real>
+template<typename T, typename Real>
+class Expansion<1,T,Real> : public ExpansionBase<1,T,Real>
 {
-  using base   = DGMatrixBase<1,Real>;
-  using array  = typename base::array;
-  using matrix = typename base::matrix;
-  static constexpr matrix _data{array{1}};
-  static constexpr auto getm(const size_t i, const size_t j) { return _data[j][i]; }
+  protected:
+    using base_type   = ExpansionBase<1,T,Real>;
+    using vector_type = typename base_type::vector_type;
+    using matrix_type = typename base_type::matrix_type;
 
-  static constexpr matrix _matrix_inv{array{1}};
-  static constexpr auto getinv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr matrix_type _matrix{vector_type{1}};
+    static constexpr matrix_type _matrix_inv{vector_type{1}};
+    static constexpr vector_type  _weight{1};
+    static constexpr vector_type  _zero{1};
 
-  static constexpr array  _weight{1};
-  static constexpr auto getw(const size_t i)                 { return _weight[i];  }
+  public:
+    static constexpr auto matrix(const size_t i, const size_t j) { return _matrix[j][i]; }
+    static constexpr auto matrix_inv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr auto weight(const size_t i) {  return _weight[i];  }
+    static constexpr auto zero(const size_t i) { return _zero[i]; }
 };
 
-template<typename Real>
-struct DGMatrix<2,Real> : public DGMatrixBase<2,Real>
+template<typename T, typename Real>
+class Expansion<2,T,Real> : public ExpansionBase<2,T,Real>
 {
-  using base   = DGMatrixBase<2,Real>;
-  using array  = typename base::array;
-  using matrix = typename base::matrix;
-  static constexpr matrix _data{
-    array{Real{1},Real{0.36602540378443865}},
-    array{Real{-1.3660254037844386},Real{1}}
-  };
-  static constexpr auto getm(const size_t i, const size_t j) { return _data[j][i]; }
+  protected:
+    using base_type   = ExpansionBase<2,T,Real>;
+    using vector_type = typename base_type::vector_type;
+    using matrix_type = typename base_type::matrix_type;
 
-  static constexpr matrix _matrix_inv{
-     array{0.6666666666666666, -0.24401693585629244},
-     array{0.9106836025229591, 0.6666666666666666}
-  };
-  static constexpr auto getinv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr matrix_type _matrix{
+      vector_type{Real{1},Real{0.36602540378443865}},
+        vector_type{Real{-1.3660254037844386},Real{1}} };
+    static constexpr matrix_type _matrix_inv{
+      vector_type{0.6666666666666666, -0.24401693585629244},
+        vector_type{0.9106836025229591, 0.6666666666666666} };
+    static constexpr vector_type  _weight{Real{0.5},Real{0.5}};
+    static constexpr vector_type  _zero{Real{1.3660254037844386}, Real{-0.36602540378443865}};
 
-  static constexpr array  _weight{Real{0.5},Real{0.5}};
-  static constexpr auto getw(const size_t i)                 { return _weight[i];  }
-
-  static constexpr array  _zerovec{Real{1.3660254037844386}, Real{-0.36602540378443865}};
-  static constexpr auto getzero(const size_t i) { return _zerovec[i]; }
+  public:
+    static constexpr auto matrix(const size_t i, const size_t j) { return _matrix[j][i]; }
+    static constexpr auto matrix_inv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr auto weight(const size_t i)  { return _weight[i];  }
+    static constexpr auto zero(const size_t i) { return _zero[i]; }
 };
 
-template<typename Real>
-struct DGMatrix<3,Real> : public DGMatrixBase<3,Real>
+template<typename T, typename Real>
+class Expansion<3,T,Real> : public ExpansionBase<3,T,Real>
 {
-  using base   = DGMatrixBase<2,Real>;
-  using array  = typename base::array;
-  using matrix = typename base::matrix;
-  static constexpr matrix _data{
-    array{ 1.1111111111111112,  0.4485512379056266, -0.08083179131550157},
-    array{-1.5596623490167376,  0.4444444444444444,  0.4485512379056266},
-    array{ 0.6363873468710571, -1.5596623490167376,  1.1111111111111112}
-  };
-  static constexpr auto getm(const size_t i, const size_t j) { return _data[j][i]; }
+  protected:
+    using base_type   = ExpansionBase<3,T,Real>;
+    using vector_type = typename base_type::vector_type;
+    using matrix_type = typename base_type::matrix_type;
 
-  static constexpr matrix _matrix_inv{
-     array{0.58, -0.18094750193111253, 0.11524199845510998},
-     array{0.9809475019311126, 0.625, -0.18094750193111253},
-     array{1.04475800154489, 0.9809475019311126, 0.58}
-  };
-  static constexpr auto getinv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr matrix_type _matrix{
+      vector_type{ 1.1111111111111112,  0.4485512379056266, -0.08083179131550157},
+        vector_type{-1.5596623490167376,  0.4444444444444444,  0.4485512379056266},
+        vector_type{ 0.6363873468710571, -1.5596623490167376,  1.1111111111111112} };
 
-  static constexpr array  _weight{
-    Real{0.2777777777777778},
-    Real{0.4444444444444444},
-    Real{0.2777777777777778}
-  };
-  static constexpr auto getw(const size_t i)                 { return _weight[i];  }
+    static constexpr matrix_type _matrix_inv{
+      vector_type{0.58, -0.18094750193111253, 0.11524199845510998},
+        vector_type{0.9809475019311126, 0.625, -0.18094750193111253},
+        vector_type{1.04475800154489, 0.9809475019311126, 0.58} };
 
+    static constexpr vector_type  _weight{
+      Real{0.2777777777777778},
+        Real{0.4444444444444444},
+        Real{0.2777777777777778} };
 
-  static constexpr array  _zerovec{
-    Real{1.4788305577012362},
-    Real{-0.6666666666666666},
-    Real{0.18783610896543051}
-  };
-  static constexpr auto getzero(const size_t i) { return _zerovec[i]; }
+    static constexpr vector_type  _zero{
+      Real{1.4788305577012362},
+        Real{-0.6666666666666666},
+        Real{0.18783610896543051} };
+
+  public:
+
+    static constexpr auto matrix(const size_t i, const size_t j) { return _matrix[j][i]; }
+    static constexpr auto matrix_inv(const size_t i, const size_t j) { return _matrix_inv[j][i]; }
+    static constexpr auto weight(const size_t i) { return _weight[i];  }
+    static constexpr auto zero(const size_t i) { return _zero[i]; }
 };
 
-#if 0
-template<typename Real>
-struct DGMatrix<3,Real> : public DGMatrixBase<3,REAL>
-{
-  static consttexpr matrix _data{
-    array{Real{1},Real{0.36602540378443865}},
-    array{Real{-1.3660254037844386},Real{1}}
-  };
-  static constexpr auto get(const size_t i, const size_t j) { return _data[j][i]; }
-};
-#endif
 
 template<typename Real>
 struct ParamT
@@ -152,6 +149,34 @@ static void compute_df(const Param &params, const Vector &f, Vector &df)
     df[i] = c * (f[i+1] - Real{2.0} * f[i] + f[i-1]);
   }
 }
+
+template<typename Param, typename Expansion, typename PDE>
+void compute_rhs_precondition(Expansion &y, const typename Expansion::value_type &y0, const Expansion &x, const PDE &pde, const Param &param)
+{
+  constexpr auto expansionOrder = Expansion::size();   // expansion order
+
+  const auto arraySize = x[0].size();
+
+  using ExpansionType = typename Expansion::value_type;
+  using Real = typename ExpansionType::value_type;
+
+  static ExpansionType diff_x;
+
+  for (int k = 0; k < expansionOrder; k++)
+  {
+    const auto weight = Expansion::weight(k);
+    const auto scale = weight * param.cfl;
+    pde(param, x[k], diff_x, [scale](const auto &val) { return val*scale;} );
+    for (int i = 0; i < arraySize; i++)
+    {
+      Real r = 0;
+      for (int l = 0; l < expansionOrder; l++)
+        r += Expansion::matrix(k,l)*x[l][i];
+      y[k][i] = x[k][i] + param.zeta*(-r + Expansion::zero(k)*y0[i] + diff_x[i]);
+    }
+  }
+}
+
 
 template<typename BC,typename Param, typename Vector>
 static void compute_update(const Param &params, Vector &f)
