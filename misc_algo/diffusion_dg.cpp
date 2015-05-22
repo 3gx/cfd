0cb166a0d69711bf73bf821ae620df216a86526a
evghenii <egaburov@dds.nl>
2015-05-22 19:29:49 +0200

    fixed parms

--- misc_algo/diffusion_dg.cpp
+++ misc_algo/diffusion_dg.cpp
@@ -316,14 +316,14 @@ class ODESolverT
           get<0>(v) = get<0>(v) + 2.0*omega*get<1>(v);
         }
 #else
-      iterate(u0, 2*2*8);
+      iterate(u0, 8*2*2*2);
 #endif
     }
 
     void solve_system(const Vector& u0)
     {
       using std::get;
-      constexpr auto niter = 2*2; // * 32; //*2; //16 ;//1; //32; //50;
+      constexpr auto niter = 8*2; // * 32; //*2; //16 ;//1; //32; //50;
       std::array<Real,Expansion::size()> error;
       for (auto iter : range_iterator{0,niter})
       {
@@ -546,7 +546,7 @@ int main(int argc, char * argv[])
   
 
 
-  constexpr auto ORDER = 3;
+  constexpr auto ORDER = 2;
   using PDE = PDEDiffusion<Real>;
   using Solver = ODESolverT<ORDER,PDE>;
 
@@ -555,7 +555,7 @@ int main(int argc, char * argv[])
 
   solver.pde().set_dx(1.0/ncell);
   solver.pde().set_diff(1);
-  solver.pde().set_cfl(0.8*64*4);  /* stable for cfl <= 0.5 */
+  solver.pde().set_cfl(0.8*64*4*4*4);  /* stable for cfl <= 0.5 */
 //  solver.pde().set_cfl(0.8);  /* stable for cfl <= 0.5 */
 
   const auto dt = solver.pde().dt();
