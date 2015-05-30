9ce5de265c1f5234765f4b2b67d50804ed78b226
evghenii <egaburov@dds.nl>
2015-05-30 21:59:08 +0200

    +1

--- osp/osp_l.py
+++ osp/osp_l.py
@@ -64,7 +64,7 @@ def minimizePoly(s,p,h,ev_space,eta,tol,maxiter=128,verbose=False,poly_guess=Non
   def cfunc2_jac(x,c,cc):
     df = np.zeros(c.shape)
     g  = np.dot(x[:-1],c[:-1]);
-    df = -np.real(c*np.conj(g) + cc*g)
+    df[:-1] = -np.real(c[:-1]*np.conj(g) + cc[:-1]*g)
     df[-1] = 1;
     return np.real(df.T)
 
